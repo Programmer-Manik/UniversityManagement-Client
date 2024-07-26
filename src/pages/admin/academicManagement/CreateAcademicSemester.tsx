be714b0d7c85ef2bea8bcaa50dc88574548817ce
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-07-27 00:37:12 +0600

    Add academic semester using RTK Query Mutation

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -6,9 +6,8 @@ import { semesterOptions } from "../../../constants/semester";
 import { monthOptions } from "../../../constants/global";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { academicSemesterSchema } from "../../../schemas/academicManagement.schema";
-
-
-
+import { useAddAcademicSemesterMutation } from "../../../redux/features/admin/academicManagementApi";
+import { toast } from "sonner";
 
 const currentYear = new Date().getFullYear();
 const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
@@ -16,28 +15,37 @@ const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
   label: String(currentYear + number),
 }));
 
-
 const CreateAcademicSemester = () => {
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    const name = semesterOptions[Number(data?.name)-1]?.label;
-   
-    const semesterData = {
-        name,
-        code: data.name,
-        year: data.year,
-        startMonth: data.startMonth,
-        endMonth: data.endMonth,
-      }; 
-      console.log(semesterData)
-  };
+  const [addAcademicSemester] = useAddAcademicSemesterMutation();
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const name = semesterOptions[Number(data?.name) - 1]?.label;
 
- 
+    const semesterData = {
+      name,
+      code: data.name,
+      year: data.year,
+      startMonth: data.startMonth,
+      endMonth: data.endMonth,
+    };
+    
+    try{
+      console.log(semesterData);
+     const res = await addAcademicSemester(semesterData)
+     console.log(res);
+    }catch(err){
+      toast.error('something went wrong ')
+    }
+
+  };
 
   return (
     <Flex justify="center" align="center">
       <Col span={6}>
-        <PHForm onSubmit={onSubmit} resolver={zodResolver(academicSemesterSchema)}>
-          <PHSelect  label="Name"  name="name" options={semesterOptions} />
+        <PHForm
+          onSubmit={onSubmit}
+          resolver={zodResolver(academicSemesterSchema)}
+        >
+          <PHSelect label="Name" name="name" options={semesterOptions} />
           <PHSelect label="Year" name="year" options={yearOptions} />
           <PHSelect
             label="Start Month"
--- src/pages/admin/academicManagement/academicSemester.tsx
+++ src/pages/admin/academicManagement/academicSemester.tsx
@@ -1,4 +1,4 @@
-import { useGetAllSemesterQuery } from "../../../redux/features/academicSemester/academicSemester";
+import { useGetAllSemesterQuery } from "../../../redux/features/admin/academicManagementApi";
 
 const AcademicSemester = () => {
     const {data} = useGetAllSemesterQuery(undefined);
